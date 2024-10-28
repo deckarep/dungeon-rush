2fc36ee38a62824e805425042c0ee9e26dfebb63
Ralph Caraveo <deckarep@gmail.com>
2024-10-27 17:03:06 -0700

    Trying to build on MacOS and Linux

--- build.zig
+++ build.zig
@@ -21,6 +21,11 @@ pub fn build(b: *std.Build) void {
     // Link C
     exe.linkSystemLibrary("c");
 
+    // This declares intent for the executable to be installed into the
+    // standard location when the user invokes the "install" step (the default
+    // step when running `zig build`).
+    b.installArtifact(exe);
+
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
